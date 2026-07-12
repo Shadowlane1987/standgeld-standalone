0b5adab6e2a433f292e2a909310dcc3632aceb9d
Markus Scharlau <m.scharlau3112@gmail.com>
2026-07-12 14:42:46 +0200

    fix(api): include all tracking cases in list

--- server/index.js
+++ server/index.js
@@ -1109,6 +1109,7 @@ app.post("/api/sixfold/standgeld", async (req, res) => {
     const filteredByDuration = recalculated.filter(
       (s) => Number(s.effective_minutes || 0) <= MAX_EFFECTIVE_MINUTES,
     );
+    const visibleStops = filteredByDuration;
     const calculated = filteredByDuration.filter(
       (s) => Number(s.billable_minutes || 0) > 0,
     );
@@ -1149,6 +1150,7 @@ app.post("/api/sixfold/standgeld", async (req, res) => {
         amount_display: formatEuro(summary.amount),
         units: summary.units,
         billed_positions: calculated.length,
+        visible_positions: visibleStops.length,
         recalculated_positions: recalculated.length,
         time_window_rows: timeWindows.length,
         time_window_matches: windowMatches,
@@ -1158,7 +1160,7 @@ app.post("/api/sixfold/standgeld", async (req, res) => {
         excluded_from_total_positions: excludedFromTotal.length,
         excluded_from_total_amount: excludedSummary.amount,
       },
-      stops: calculated,
+      stops: visibleStops,
       note: null,
     });
   } catch (error) {
